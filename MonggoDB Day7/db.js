7fa5d405ed7c9bcf1230549f15561043fcb3a257
Indra Ardiyanto <114069365+zettacamp-indra-ardiyanto@users.noreply.github.com>
2022-10-31 17:24:15 +0700

    monggodb 7

---
+++ MonggoDB Day7/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const dbURI = 'mongodb://localhost:27017/bookDb';
+
+mongoose.connect(dbURI, {useNewUrlParser:true})
+// connect to database
+const db = mongoose.connection
+// if error
+db.on("error", (err) => {
+  console.error(`err: ${err}`)
+})// if connected
+db.on('connected', (err, res) => {
+  console.log('Connected to database')
+})
